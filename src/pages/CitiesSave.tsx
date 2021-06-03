212f92dffb0578cd0cfe9c89fc0da812ec5fcf26
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-03 11:30:07 -0300

    feat(navigation): add initial app pages

---
+++ src/pages/CitiesSave.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function CitiesSave() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>CitiesSave</Text>
+    </View>
+  );
+}

5e2d879c5d36da598760063a88cd9962fc246c3c
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-02 22:11:11 -0300

    add colors config file

---
+++ src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  white: "#ffffff",
+  light: "#f5f5f5",
+  lightBlue: "#00aaf2",
+  blue: "#0078be",
+  orange: "#f28200",
+  dark87: "#de000000",
+  dark60: "#99000000",
+}

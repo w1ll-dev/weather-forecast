a9275c685d5e3141dd3e4df343f286044ed0d731
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-04 16:50:36 -0300

    feat(search-screen): apply Container common component in Main component

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { Routes } from './routes';
-import { Container } from './styles/components/common';
+import { Container } from './styles/components/Common';
 import { colors } from './styles';
 
 export function Main() {

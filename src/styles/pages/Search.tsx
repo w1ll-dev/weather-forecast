0afbee32255708fcd34b73ac1f75fb6ccfcc7422
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-04 23:45:09 -0300

    feat(search-screen): apply fix full size to flatlist cities container

--- src/styles/pages/Search.tsx
+++ src/styles/pages/Search.tsx
@@ -2,5 +2,6 @@ import styled from 'styled-components/native';
 import { verticalPixel } from '../../hooks/pixelConvert';
 
 export const Content = styled.View`
+  flex: 1;
   padding: ${verticalPixel(16)}px;
 `;

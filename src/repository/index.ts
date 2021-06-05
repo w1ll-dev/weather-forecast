3414a7153d5ecbb200ae06cdac354a0c33f21fa8
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-04 23:07:57 -0300

    feat(search-screen): create getPlacesAutocomplete method

---
+++ src/repository/index.ts
@@ -0,0 +1,33 @@
+import { requestAdapter } from './adapters/requestAdapter';
+import { googleApibaseUrl } from './constants';
+import {
+  PlaceAutocompleteApiResponseProtocol,
+  PlaceAutocompleteProtocol,
+} from './protocols';
+import { GOOGLE_SEARCH_API_KEY } from '@env';
+import { PlaceAutocomplete } from './entities';
+import { ServerError } from './errors/ServerError';
+
+export async function getPlacesAutocomplete(
+  input: string
+): Promise<PlaceAutocompleteProtocol[]> {
+  const inputFixed = input.replace(/\s+/g, '+');
+  const query = `${googleApibaseUrl}/autocomplete/json?input=${inputFixed}&types=(cities)&language=pt-br&key=${GOOGLE_SEARCH_API_KEY}`;
+  try {
+    const response: PlaceAutocompleteApiResponseProtocol = await requestAdapter(
+      query
+    );
+    const { predictions } = response;
+    const placesList = predictions.map(
+      ({ terms, place_id }) =>
+        new PlaceAutocomplete(
+          place_id,
+          terms[0].value,
+          terms[terms.length - 1].value
+        )
+    );
+    return placesList;
+  } catch (error) {
+    throw new ServerError();
+  }
+}

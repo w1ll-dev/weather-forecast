9eac1fab628965a982bc7c2dec2c0b013d0bb9de
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-05 12:26:06 -0300

    feat(cities-save-screen): add openweatherApibaseUrl to repository constants

--- src/repository/constants.ts
+++ src/repository/constants.ts
@@ -1,2 +1,4 @@
+export const openweatherApibaseUrl =
+  'https://api.openweathermap.org/data/2.5/onecall';
 export const googleApibaseUrl = 'https://maps.googleapis.com/maps/api/place';
 export const citiesStorageKey = '@weatherforecast:cities';

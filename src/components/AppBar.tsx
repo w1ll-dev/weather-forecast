142360dabfac4d552627dc8bdab0a4b4625cb0be
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-05 15:12:08 -0300

    feat(cities-save-screen): add navigation to appbar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,13 +1,20 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { pt } from '../constants';
+import { pages, pt } from '../constants';
 import { Title, Wrapper, SearchIcon } from '../styles/components/AppBar';
 
 export default function AppBar() {
+  const navigation = useNavigation();
+
+  function handleClick() {
+    navigation.navigate(pages.SEARCH);
+  }
+
   return (
     <Wrapper>
       <Title>{pt.appBarTitle}</Title>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleClick}>
         <SearchIcon />
       </TouchableOpacity>
     </Wrapper>

cbc69d748fde754d45ec1c884b58240f45b21b07
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-05 15:23:37 -0300

    feat(cities-save-screen): add initial SearchCityCard component to initial CitiesSave page

--- src/pages/CitiesSave.tsx
+++ src/pages/CitiesSave.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import React, { useEffect, useState } from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+import { SearchCityCard } from '../components';
+import AppBar from '../components/AppBar';
+import { PlaceAutocomplete } from '../constants';
+import { loadStoragePlaces } from '../libs';
+import { getPlaceWeatherForecast } from '../repository';
+import { Container, Content } from '../styles/components/Common';
 
 export function CitiesSave() {
+  const [placesSaved, setPlacesSaved] = useState<PlaceAutocomplete[]>([]);
+
+  async function loadCities() {
+    const placesSaved = await loadStoragePlaces();
+    const placeWithCompleteInfoList = [];
+    for (const place of placesSaved) {
+      const placeWithCompleteInfo = await getPlaceWeatherForecast(place);
+      placeWithCompleteInfoList.push(placeWithCompleteInfo);
+    }
+    setPlacesSaved(placeWithCompleteInfoList);
+  }
+
+  useEffect(() => {
+    loadCities();
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>CitiesSave</Text>
-    </View>
+    <Container>
+      <AppBar />
+      <Content>
+        {placesSaved.length === 0 ? (
+          <AppLoading />
+        ) : (
+          <FlatList
+            data={placesSaved}
+            showsVerticalScrollIndicator={false}
+            numColumns={1}
+            keyExtractor={({ place_id }) => place_id}
+            initialNumToRender={12}
+            maxToRenderPerBatch={12}
+            windowSize={5}
+            renderItem={({ item }: { item: PlaceAutocomplete }) => (
+              <SearchCityCard
+                cityName={item.city}
+                country={item.country}
+                key={item.place_id}
+                saveCity={() => {}}
+              />
+            )}
+          />
+        )}
+      </Content>
+    </Container>
   );
 }

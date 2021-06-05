c76e46f34061eb6744c305d89cd2dc94b71bbc09
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-05 12:25:01 -0300

    refactor(dead-code): remove dead import

--- src/hooks/usePlacesAutocomplete.ts
+++ src/hooks/usePlacesAutocomplete.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getPlacesAutocomplete } from '../repository';
 import { PlaceAutocomplete } from '../constants/entities';
 import { useDebounce } from './useDebounce';

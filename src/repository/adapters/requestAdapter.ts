31db661a18eef86d7cf641c84d6de411c3214024
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-04 23:02:35 -0300

    feat(search-screen): create request adapter
    With this request adapter we can change how we make our requests, as example with this we can
    replace fetch api for axios with no problem.

---
+++ src/repository/adapters/requestAdapter.ts
@@ -0,0 +1,7 @@
+import { googleApibaseUrl } from '../constants';
+
+export async function requestAdapter(query: string): Promise<any> {
+  const response = await fetch(query);
+  const responseJson = await response.json();
+  return responseJson;
+}

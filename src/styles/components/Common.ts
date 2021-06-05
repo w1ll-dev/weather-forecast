5eddf40b3e0cdd47b1dac8b5f4ef65d73d1635a9
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-05 12:06:04 -0300

    refactor(search-screen): remove content from Search screen style and move to common styles

--- src/styles/components/Common.ts
+++ src/styles/components/Common.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components/native';
+import { verticalPixel } from '../../hooks/pixelConvert';
 
 export const Container = styled.View`
   flex: 1;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  padding: ${verticalPixel(16)}px;
 `;
--- src/styles/pages/Search.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components/native';
-import { verticalPixel } from '../../hooks/pixelConvert';
-
-export const Content = styled.View`
-  flex: 1;
-  padding: ${verticalPixel(16)}px;
-`;

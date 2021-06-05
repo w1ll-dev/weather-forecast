b41e15afbdc60837c9358c713da58ee09a0c5bc4
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-05 11:21:14 -0300

    feat(save-city): add save city action to SearchCityCard

--- src/components/SearchCityCard.tsx
+++ src/components/SearchCityCard.tsx
@@ -13,16 +13,21 @@ import { pt } from '../constants/translate';
 interface SearchCityCardProps {
   cityName: string;
   country: string;
+  saveCity: any;
 }
 
-export function SearchCityCard({ cityName, country }: SearchCityCardProps) {
+export function SearchCityCard({
+  cityName,
+  country,
+  saveCity,
+}: SearchCityCardProps) {
   return (
     <Wrapper style={boxShadow}>
       <CityInfo>
         <CityLabel>{cityName}</CityLabel>
         <CountryLabel>{country}</CountryLabel>
       </CityInfo>
-      <AddCityButton>
+      <AddCityButton onPress={saveCity}>
         <AddCityLabel>{pt.addCityLabel}</AddCityLabel>
       </AddCityButton>
     </Wrapper>

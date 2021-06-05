2ce572a7d58c1c25e7da5ebcc7fed049944447bf
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-04 23:42:57 -0300

    refactor(dead-code): remove unused import

--- src/repository/adapters/requestAdapter.ts
+++ src/repository/adapters/requestAdapter.ts
@@ -1,5 +1,3 @@
-import { googleApibaseUrl } from '../constants';
-
 export async function requestAdapter(query: string): Promise<any> {
   const response = await fetch(query);
   const responseJson = await response.json();

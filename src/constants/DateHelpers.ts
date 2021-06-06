813b65aef5e100295f5480c12b579362a8b38d99
w1ll-dev <w1llmarcio.dev@hotmail.com>
2021-06-06 02:30:17 -0300

    feat(city-weather-forecast): add date helpers

---
+++ src/constants/DateHelpers.ts
@@ -0,0 +1,26 @@
+import { pt } from './translate';
+
+export const daysOfTheWeek = [
+  pt.days.sundayLabel,
+  pt.days.mondayLabel,
+  pt.days.tuesdayLabel,
+  pt.days.wednesdayLabel,
+  pt.days.thrusdayLabel,
+  pt.days.fridayLabel,
+  pt.days.saturdayLabel,
+];
+
+export const months = [
+  pt.months.january,
+  pt.months.february,
+  pt.months.march,
+  pt.months.april,
+  pt.months.may,
+  pt.months.june,
+  pt.months.july,
+  pt.months.august,
+  pt.months.september,
+  pt.months.october,
+  pt.months.november,
+  pt.months.december,
+];
